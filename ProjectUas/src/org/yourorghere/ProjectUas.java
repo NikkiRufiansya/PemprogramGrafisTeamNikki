90a2a1e6bad066e4c691dba19db352f0692ff331
Ashlihlana <aslihqurotaayuni@gmail.com>
2018-06-05 22:34:53 +0700

    Update ProjectUas.java

--- ProjectUas/src/org/yourorghere/ProjectUas.java
+++ ProjectUas/src/org/yourorghere/ProjectUas.java
@@ -1,7 +1,11 @@
 package org.yourorghere;
 
 import com.sun.opengl.util.Animator;
+import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.media.opengl.GL;
@@ -11,20 +15,13 @@ import javax.media.opengl.GLEventListener;
 import javax.media.opengl.glu.GLU;
 
 
-
-/**
- * ProjectUas.java <BR>
- * author: Brian Paul (converted to Java by Ron Cemer and Sven Goethel) <P>
- *
- * This version is equal to Brian Paul's version 1.2 1999/10/21
- */
-public class ProjectUas implements GLEventListener {
+public class ProjectUas implements GLEventListener, MouseListener, MouseMotionListener {
 
     public static void main(String[] args) {
-        Frame frame = new Frame("Coba testing");
+        Frame frame = new Frame("Project UAS");
         GLCanvas canvas = new GLCanvas();
 
-        canvas.addGLEventListener(new ProjectUas());
+        canvas.addGLEventListener(new Pertemuan4());
         frame.add(canvas);
         frame.setSize(640, 480);
         final Animator animator = new Animator(canvas);
@@ -49,20 +46,46 @@ public class ProjectUas implements GLEventListener {
         frame.setVisible(true);
         animator.start();
     }
+    private float view_rotx = 0.0f;
+    private float view_roty = 0.0f;
+    private int oldMouseX;
+    private int oldMouseY;
 
     public void init(GLAutoDrawable drawable) {
-        // Use debug pipeline
-        // drawable.setGL(new DebugGL(drawable.getGL()));
-
         GL gl = drawable.getGL();
-        System.err.println("INIT GL IS: " + gl.getClass().getName());
-
-        // Enable VSync
-        gl.setSwapInterval(1);
+//        System.err.println("INIT GL IS: " + gl.getClass().getName());
+//
+//        // Enable VSync
+        //gl.setSwapInterval(1);
 
         // Setup the drawing area and shading mode
-        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-        gl.glShadeModel(GL.GL_SMOOTH); // try setting this to GL_FLAT and see what happens.
+        gl.glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+        gl.glShadeModel(GL.GL_FLAT); // try setting this to GL_FLAT and see what happens.
+        float ambient[] = {1.0f, 1.0f, 1.0f, 1.0f};
+        float difusse[] = {1.0f, 1.0f, 1.0f, 1.0f};
+        float specular[] = {0.2f, 1.0f, 0.2f, 1.0f};
+        float position[] = {20.0f, 30.0f, 20.0f, 0.0f};
+
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT, ambient, 0);
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, difusse, 0);
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, position, 0);
+        gl.glMaterialfv(GL.GL_FRONT, GL.GL_SPECULAR, specular, 0);
+        float[] mambient = {0.1745f, 0.01175f, 0.01175f, 0.55f
+        };
+        float[] mdiffuse = {0.61424f, 0.04136f, 0.04136f, 0.55f};
+        float[] mspecular = {0.727811f, 0.626959f, 0.626959f, 0.55f};
+        float mshine = 76.8f;
+
+        gl.glMaterialfv(GL.GL_FRONT, GL.GL_AMBIENT, mambient, 0);
+        gl.glMaterialfv(GL.GL_FRONT, GL.GL_DIFFUSE, mdiffuse, 0);
+        gl.glMaterialfv(GL.GL_FRONT, GL.GL_SPECULAR, mspecular, 0);
+        gl.glMaterialf(GL.GL_FRONT, GL.GL_SHININESS, mshine);
+        gl.glEnable(GL.GL_LIGHTING);
+        gl.glEnable(GL.GL_LIGHT0);
+        gl.glEnable(GL.GL_DEPTH_TEST);
+        gl.glEnable(GL.GL_NORMALIZE);
+        drawable.addMouseListener(this);
+        drawable.addMouseMotionListener(this);
     }
 
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
@@ -70,7 +93,7 @@ public class ProjectUas implements GLEventListener {
         GLU glu = new GLU();
 
         if (height <= 0) { // avoid a divide by zero error!
-        
+
             height = 1;
         }
         final float h = (float) width / (float) height;
@@ -84,43 +107,57 @@ public class ProjectUas implements GLEventListener {
 
     public void display(GLAutoDrawable drawable) {
         GL gl = drawable.getGL();
-
+        GLU glu = new GLU();
         // Clear the drawing area
         gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
         // Reset the current matrix to the "identity"
         gl.glLoadIdentity();
+        glu.gluLookAt(8, 8, 8, 0, 0, 0, 0, 0, 1);
 
-        // Move the "drawing cursor" around
-        gl.glTranslatef(-1.5f, 0.0f, -6.0f);
-
-        // Drawing Using Triangles
-        gl.glBegin(GL.GL_TRIANGLES);
-            gl.glColor3f(1.0f, 0.0f, 0.0f);    // Set the current drawing color to red
-            gl.glVertex3f(0.0f, 1.0f, 0.0f);   // Top
-            gl.glColor3f(0.0f, 1.0f, 0.0f);    // Set the current drawing color to green
-            gl.glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom Left
-            gl.glColor3f(0.0f, 0.0f, 1.0f);    // Set the current drawing color to blue
-            gl.glVertex3f(1.0f, -1.0f, 0.0f);  // Bottom Right
-        // Finished Drawing The Triangle
-        gl.glEnd();
-
-        // Move the "drawing cursor" to another position
-        gl.glTranslatef(3.0f, 0.0f, 0.0f);
-        // Draw A Quad
-        gl.glBegin(GL.GL_QUADS);
-            gl.glColor3f(0.5f, 0.5f, 1.0f);    // Set the current drawing color to light blue
-            gl.glVertex3f(-1.0f, 1.0f, 0.0f);  // Top Left
-            gl.glVertex3f(1.0f, 1.0f, 0.0f);   // Top Right
-            gl.glVertex3f(1.0f, -1.0f, 0.0f);  // Bottom Right
-            gl.glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom Left
-        // Done Drawing The Quad
-        gl.glEnd();
-
-        // Flush all drawing operations to the graphics card
-        gl.glFlush();
+        // Objek Bola         
+        gl.glTranslatef(1.0f,1.0f,1.0f);         
+        gl.glPushMatrix();         
+        Obj.Bola(gl); // Pada fungsi yang sudah dibuat pada pembahasan sebelumnya         
+        gl.glPopMatrix();                  
+        
+       
+        gl.glFlush(); 
     }
 
     public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
     }
-}
 
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {
+    }
+
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    public void mousePressed(MouseEvent e) {
+        oldMouseX = e.getX();
+        oldMouseY = e.getY();
+    }
+
+    public void mouseDragged(MouseEvent e) {
+        int x = e.getX();
+        int y = e.getY();
+        Dimension size = e.getComponent().getSize();
+        float thetaY = 360.0f * ((float) (x - oldMouseX) / (float) size.width);
+        float thetaX = 360.0f * ((float) (oldMouseY - y) / (float) size.height);
+        oldMouseX = x;
+        oldMouseY = y;
+        view_rotx += thetaX;
+        view_roty += thetaY;
+
+    }
+
+    public void mouseMoved(MouseEvent e) {
+
+    }
+}
